73e67a4a4445dd82cb66e917da5d66cb73bd886e
WairiaGracee <gjblessed12@gmail.com>
2025-06-13 07:16:32 +0300

    setting up mongoDB

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
-require('dotenv').config();
+require('dotenv').config(); //loads the environment variables form .env into process.env
+
+const express = require('express');//import express
+const mongoose = require('mongoose');//import mongoose for mongoDB
+
+const app = express();//initialize express
+const PORT = process.env.PORT || 5000;//tells the app what port it runs on
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log('MongoDB Connected 🎉'))
+.catch(err => console.error('MongoDB connection error:', err));
+
+
