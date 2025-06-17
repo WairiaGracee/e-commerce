b28eeb734284acef5c52abf4bed0fd8390bf3103
WairiaGracee <gjblessed12@gmail.com>
2025-06-18 00:02:27 +0300

    ddd

--- client/functionality/homepage/header.js
+++ client/functionality/homepage/header.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
+    const overlay = document.querySelector('.overlay');
     const header = document.getElementById('header')
     const navLinks = document.querySelector('.nav-links')
     const cart = document.querySelector('.carthide')
@@ -6,10 +7,20 @@ document.addEventListener('DOMContentLoaded', function(){
     const logo = document.querySelector('.logo')
 
     menu.addEventListener('click', function(){
-        cart.classList.toggle('hide')
-        navLinks.style.display = 'flex'
-        menu.classList.toggle('hide')
-        logo.classList.toggle('hide')
+        overlay.classList.add('active');
+        navLinks.classList.add('active');
+        logo.classList.add('hide');
+        cart.classList.add('hide');
     })
+
+    // Click outside sidebar to close
+    overlay.addEventListener('click', function(e) {
+        if (!navLinks.contains(e.target)) {
+        overlay.classList.remove('active');
+        navLinks.classList.remove('active');
+        logo.classList.remove('hide');
+        cart.classList.remove('hide');
+        }
+    });
 
 })
